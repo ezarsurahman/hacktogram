a32f8226a3ed58c0be995f8397bdbcbd932eeced
Ezar Surahman <akhdanezar@gmail.com>
2024-07-30 17:07:00 +0700

    finisih route on App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute"
+import Photos from "./routes/Photos"
+import Home from "./layout/Home.jsx"
+import CreatePhoto from "./routes/CreatePhoto.jsx"
+import Detail from "./routes/Detail.jsx"
+import SignIn from "./routes/SignIn.jsx"
+import Register from "./routes/Register.jsx"
 // TODO: answer here
 
 const App = () => {
     return (
-        <></> // TODO: replace this
+        <Routes>
+            <Route path="/" element={<ProtectedRoute>
+                <Home></Home>
+            </ProtectedRoute>}>
+                <Route path="index" element={<Photos></Photos>}></Route>
+                <Route path="create" element={<CreatePhoto></CreatePhoto>}></Route>
+                <Route path=":id" element={<Detail></Detail>}></Route>
+            </Route>
+
+            <Route path="/signin" element={<SignIn></SignIn>}></Route>
+            <Route path="/register" element={<Register></Register>}></Route>
+        </Routes> // TODO: replace this
     );
 };
 
